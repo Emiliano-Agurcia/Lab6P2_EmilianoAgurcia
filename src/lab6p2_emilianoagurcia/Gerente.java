64dc98a4def9ab4ba7a685f8e7d37b33154e75ac
Emiliano-Agurcia <110260889+Emiliano-Agurcia@users.noreply.github.com>
2022-08-26 13:27:59 -0600

    Commit 1

---
+++ src/lab6p2_emilianoagurcia/Gerente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6p2_emilianoagurcia;
+
+/**
+ *
+ * @author emili
+ */
+public class Gerente extends Persona{
+    String Usuario;
+    String Password;
+    String Cargo;
+}
